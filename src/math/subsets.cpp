2a65e0c95bbb7a076df25cf8661a930cec0ae47c
Your Name <email@example.com>
2020-04-05 16:22:16 +0800

    二元组生成集合

---
+++ src/math/subsets.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> res;
+        for(int i=0;i<(1<<nums.size());i++){、
+        //1左移size位得到2的size次方，枚举0-2^(size)-1,
+            vector<int> now;
+            for(int j=0;j<nums.size();j++){
+                if((i>>j)&1)now.push_back(nums[j]);
+                //看当前数二进制哪个位置上有1，则将对应位置的数填充进去数组
+            }
+            res.push_back(now);
+        }
+        return res;
+    }
+};
